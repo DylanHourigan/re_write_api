62f0031c321bb72c9588938874209d4ac0bd00df
Dylan Hourigan <dylan.a.hourigan@gmail.com>
2023-08-05 19:29:27 +0100

    Feat: Added routing and rule_based paraphrasing model

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
-from models.rule_based import paraphrase
-app = Flask(__name__)
+from routes.rule_based import bp as paraphrase_bp
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def create_app():
+    app = Flask(__name__)
+
+    # Register the blueprints
+    app.register_blueprint(paraphrase_bp)
+
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
---
+++ routes/rule_based.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, request, jsonify
+from services.rule_based import paraphrase
+
+bp = Blueprint('paraphrase', __name__, url_prefix='/paraphrase')
+
+@bp.route('/', methods=['POST'])
+def paraphraseV1():
+    data = request.get_json()
+    if not data:
+        return jsonify({"message": "No input data provided"}), 400
+    try:
+        result = paraphrase(data['input'])
+        return jsonify({"paraphrase": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
