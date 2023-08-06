4e8c1235061bce3a8025ace4ed3b64855b323e58
Dylan Hourigan <dylan.a.hourigan@gmail.com>
2023-08-06 19:37:27 +0100

    Added heroku config

--- app.py
+++ app.py
@@ -11,5 +11,6 @@ def create_app():
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
