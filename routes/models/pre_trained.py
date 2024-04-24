b1712cf7e3cf9f2070bbe95095eb4fe3a31d061b
DylanHourigan <dylan.a.hourigan@gmail.com>
2024-04-24 22:19:19 +0100

    Added endpoint for multiple results to be output

--- routes/models/pre_trained.py
+++ routes/models/pre_trained.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from services.models.pre_trained import paraphrase
+from services.models.pre_trained import paraphrase, paraphrase_all
 
 bp = Blueprint('paraphraseV2', __name__, url_prefix='/V2/paraphrase')
 
@@ -13,3 +13,11 @@ def paraphraseV2():
         return jsonify({"paraphrase": result}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+    
+@bp.route('/multiple', methods=['POST'])
+def paraphrase_multiple():
+    data = request.get_json()
+    text = data['input']
+    paraphrases = paraphrase_all(text)
+    return jsonify({'paraphrases': paraphrases}), 200
--- services/models/pre_trained.py
+++ services/models/pre_trained.py
@@ -21,3 +21,24 @@ def paraphrase(text):
     paraphrases = [tokenizer.decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=True) for output in paraphrased_output]
         
     return random.choice(paraphrases)
+
+def paraphrase_all(text):
+    encoding = tokenizer.encode_plus(text,pad_to_max_length=True, return_tensors="pt")
+    input_ids, attention_masks = encoding["input_ids"].to("cpu"), encoding["attention_mask"].to("cpu")
+    
+    paraphrased_output = model.generate(
+        input_ids=input_ids, attention_mask=attention_masks,
+        max_length=256,
+        do_sample=True,
+        top_k=120,
+        top_p=0.95,
+        early_stopping=True,
+        num_return_sequences=15
+        )
+    paraphrases = set() 
+    for output in paraphrased_output:
+        paraphrase = tokenizer.decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+        if paraphrase != text:
+            paraphrases.add(paraphrase)
+
+    return list(paraphrases)[:5]
